04fbb5c0cc570d43a6855e4ebf73346add9184e1
Abhi Ladukodi <ladukodi.yeet@gmail.com>
2021-02-13 09:46:55 -0600

    made name changes to index file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import flask, render
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 app.route('/results')
 def home():
